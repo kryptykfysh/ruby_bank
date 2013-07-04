d1008b3ecea12f125d998c63dd0c36cdc47f9d55
KryptykFysh <kryptykfysh@kryptykfysh.co.uk>
2013-07-04 13:38:03 +1000

    All methods and tests for Bank class written.

--- lib/ruby_bank/bank.rb
+++ lib/ruby_bank/bank.rb
@@ -1,13 +1,18 @@
+require 'bigdecimal'
+
 module RubyBank
   class Bank
     attr_reader :name, :assets, :accounts, :liability
 
     def initialize(name)
-
+      @name = name
+      @assets = []
+      @accounts = []
+      @liability = BigDecimal.new('0')
     end
 
-    def open_an_account
-
+    def open_an_account(name: nil, hut_number: nil)
+      self.accounts << {name: name, hut_number: hut_number}
     end
   end
 end
--- spec/ruby_bank/bank_spec.rb
+++ spec/ruby_bank/bank_spec.rb
@@ -16,5 +16,62 @@ module RubyBank
       it { should respond_to(:liability) }
       it { should respond_to(:open_an_account) }      
     end
+
+    describe '#new' do
+      context 'with a valid String argument' do
+        subject { @bank }
+
+        it { should be_an_instance_of(Bank) }
+      end
+
+      context 'with no name argument' do
+        it 'should raise an ArgumentError' do
+          expect{ RubyBank::Bank.new }.to raise_error(ArgumentError)
+        end
+      end
+    end
+
+    describe '#name' do
+      subject { @bank.name }
+
+      it { should be_an_instance_of(String) }
+
+      context "with 'Test Bank' as the :name" do
+        it { should eql 'Test Bank' }
+      end
+    end
+
+    describe '#assets' do
+      # If have no idea what #assets should be returning.
+      # I'm just guessing here.
+      subject { @bank.assets }
+
+      it { should be_an_instance_of(Array) }
+    end
+
+    describe '#accounts' do
+      # If have no idea what #accounts should be returning.
+      # I'm just guessing it should be an Array of Accounts.
+      subject { @bank.accounts }
+
+      it { should be_an_instance_of(Array) }
+    end
+
+    describe '#liability' do
+      # If have no idea what #liability should be returning.
+      # I'm just guessing it's a BigDecimal.
+      subject { @bank.liability }
+
+      it { should be_an_instance_of(BigDecimal) }
+    end
+
+    describe '#open_an_account' do
+      context 'with valid :name and :hut_number arguments' do
+        it 'should increase size of :accounts by 1' do
+          expect{ @bank.open_an_account(name: 'Test Client', hut_number: 'HUT0001') }.
+            to change{@bank.accounts.size}.from(0).to(1)
+        end
+      end
+    end
   end
 end
